66093e79fb2779f88d57fa7efeb751354d5af9af
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-03 22:07:27 +0400

    user commented

--- resources/main.tf
+++ resources/main.tf
@@ -26,7 +26,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
 }
 
 
-
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_ports_firewall_roboshop.id
   cidr_ipv4         = "0.0.0.0/0"
